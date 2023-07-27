8d3dfef9c33ea96b58c3fd998273e9ec3ed10c50
preethaneesh <anupampree.a@gmail.com>
2023-07-27 14:27:35 +0530

    <Message>

---
+++ school_store_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name
